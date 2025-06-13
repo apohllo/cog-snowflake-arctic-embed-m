e4ab19d77df70c0397c0c8f36f8b40cc147a5667
Aleksander Smywiński-Pohl <apohllo@o2.pl>
2025-06-13 15:02:58 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@
 
 from cog import BasePredictor, Input
 from typing import List
-from transformers import AutoModel, AutoTokenizer
+from sentence_transformers import SentenceTransformer
 
 MODEL_ID = "Snowflake/snowflake-arctic-embed-m-v2.0"
 MODEL_CACHE = "checkpoint"
@@ -11,14 +11,15 @@ MODEL_CACHE = "checkpoint"
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=MODEL_CACHE)
-        self.model = AutoModel.from_pretrained(MODEL_ID, add_pooling_layer=False, cache_dir=MODEL_CACHE)
+        self.model = SentenceTransformer(MODEL_ID, trust_remote_code=True, cache_folder=MODEL_CACHE)
 
     def predict(
         self,
         documents: List[str] = Input(description="Document to create the embeddings for", default=['Snowflake is the Data Cloud!']),
+        query: bool = Input(description="Whether to add query prefix"", default=False),
     ) -> List[float]:
-        encoded_input = self.tokenizer(documents, padding=True, return_tensors='pt')
-        outputs = self.model(**encoded_input).last_hidden_state
-        embeddings = outputs[:, 0].tolist()
+        if(query):
+            embeddings = self.model.encode(documents, prompt_name="query")
+        else:
+            embeddings = self.model.encode(documents)
         return embeddings
